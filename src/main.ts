35ee2ad678e2db107f8b2c0b44bce84ebeb278e9
xitu <dili.li@outlook.com>
2023-07-08 00:09:11 +0800

    feat(*): 新增 ElementPlusIcons

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +15,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.mount('#app')
