5da1669b73b61dfe74cf3795f06cf3664d346184
xitu <dili.li@outlook.com>
2023-07-29 18:27:09 +0800

    feat(User): 新增 重新加载页面时mySelf数据恢复逻辑

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import { onMounted, watch } from 'vue'
 import { useRouter } from 'vue-router'
+import { useUserStore } from './stores/user'
 import { useHeaderStore } from './stores/header'
 import { useTechStackStore } from './stores/tech-stack'
 import Header from './components/RootHeader.vue'
 import UserLogin from './components/UserLogin.vue'
 import Footer from './components/footer/RootFooter.vue'
 import RBCornerContainer from './components/corner/RBCornerContainer.vue'
+import { error } from './utils/logger'
 
 const router = useRouter()
+const userStore = useUserStore()
 const headerStore = useHeaderStore()
 
 watch(router.currentRoute, (newRouter) => {
@@ -21,6 +24,17 @@ watch(router.currentRoute, (newRouter) => {
 onMounted(() => {
   const { setTechStack } = useTechStackStore()
   setTechStack('home')
+
+  // 根据Session存储恢复登录状态
+  const mySelfStr = sessionStorage.getItem('mySelf')
+  try {
+    const mySelf = JSON.parse(mySelfStr)
+    if (mySelf && mySelf?.id !== 0) {
+      userStore.mySelf = mySelf
+    }
+  } catch (err) {
+    error('JSON解析错误:', err)
+  }
 })
 </script>
 
